156605f0ac8319a646a1c1e84fdb79db8d03ba67
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-08-09 20:54:16 -0300

    [feat] add atributes and methods to Conta and ContaEspecial

--- src/Conta.java
+++ src/Conta.java
@@ -1,3 +1,59 @@
+import java.util.ArrayList;
+
 public class Conta {
-    //TO DO
+    //atributos
+    protected int id;
+    protected String chavePiks;
+    protected double saldo;
+    protected Cliente cliente;
+    protected ArrayList<Lancamento> lancamentos = new ArrayList<Lancamento>();
+
+    //construtores
+    public Conta(int id, String chavePiks, double saldo, Cliente cliente){
+        this.id = id;
+        this.chavePiks = chavePiks;
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    public Conta(int id, String chavePiks, Cliente cliente){
+        this.id = id;
+        this.chavePiks = chavePiks;
+        this.saldo = 0;
+        this.cliente = cliente;
+    }
+
+    public Conta(int id, Cliente cliente){
+        this.id = id;
+        this.saldo = 0;
+        this.cliente = cliente;
+    }
+
+    //getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public String getChavePiks() {
+        return chavePiks;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    //métodos gerais
+    public void creditar(double valor){
+        this.saldo += valor;
+        //this.lancamentos.add(new Lancamento(valor, "+")); //descomentar esta linha depois de implementar a classe Lancamento, e seu construtor
+    }
+
+    public void debitar(double valor) throws Exception{
+        if(valor > this.saldo){
+            throw new Exception("valor debitado não pode ser maior que o saldo");
+        }
+        
+        this.saldo -= valor;
+        //this.lancamentos.add(new Lancamento(valor, "-")); //descomentar esta linha depois de implementar a classe Lancamento, e seu construtor
+    }
 }
--- src/ContaEspecial.java
+++ src/ContaEspecial.java
@@ -1,3 +1,31 @@
-public class ContaEspecial {
-    //TO DO
+public class ContaEspecial extends Conta{
+    //atributo
+    private double limite;
+
+    //construtores
+    public ContaEspecial(int id, String chavePiks, double saldo, Cliente cliente, double limite) {
+        super(id, chavePiks, saldo, cliente);
+        this.limite = limite;
+    }
+
+    public ContaEspecial(int id, String chavePiks, Cliente cliente, double limite) {
+        super(id, chavePiks, cliente);
+        this.limite = limite;
+    }
+
+    public ContaEspecial(int id, Cliente cliente, double limite) {
+        super(id, cliente);
+        this.limite = limite;
+    }
+
+    //método
+    @Override
+    public void debitar(double valor) throws Exception{
+        if(valor > (this.saldo + this.limite) ){
+            throw new Exception("esta transferência excede o limite");
+        }
+
+        this.saldo -= valor;
+        //this.lancamentos.add(new Lancamento(valor, "-")); //descomentar esta linha quando implementar a classe Lancamento, e seu construtor
+    }
 }
