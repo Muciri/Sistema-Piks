5c6a85b842a00ee70a64835c388135a21a1f2f67
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-08-15 10:42:01 -0300

    [feat] add atributes and methods to Repositorio class

--- src/Repositorio.java
+++ src/Repositorio.java
@@ -1,3 +1,48 @@
+import java.util.TreeMap;
+
 public class Repositorio {
-    //TO DO
+    //atributos
+    private static TreeMap<String,Conta> contasPIKS = new TreeMap<String,Conta>();
+    private static TreeMap<Integer, Cliente> clientesCPF = new TreeMap<Integer, Cliente>();
+
+    //construtor privado, por ser uma classe utilitária
+    private Repositorio(){};
+
+    //bloco estático de inicialização
+    static {
+        lerObjetos();
+    }
+
+    //getters e setters
+
+    //métodos gerais
+    public static void adicionarConta(Conta conta) {
+        contasPIKS.put(conta.toString(), conta);
+    }
+
+    public static void removerConta(Conta conta) {
+        contasPIKS.remove(conta.toString());
+    }
+
+    public static Conta localizarConta(String chavePiks){
+        for(Conta conta: contasPIKS.values()) {
+            if(conta.getChavePiks().equals(chavePiks)){
+                return conta;
+            }
+        }
+
+        return null; //se a conta não for encontrada, retorna null
+    }
+
+    public static void adicionarCliente(Cliente cliente){
+        //clientesCPF.put(cliente.getCPF(), cliente); //descomentar esta linha quando implementar a classe cliente e seus metodos
+    }
+
+    public static void removerCliente(Cliente cliente){
+        //clientesCPF.remove(cliente.getCPF()); //descomentar esta linha quando implementar a classe cliente e seus metodos
+    }
+
+    public static void lerObjetos(){
+        //TO DO
+    }
 }
